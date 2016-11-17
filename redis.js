67b2c6e8e1618fe4c9bcb15302fe65ac77bac19e
Wyatt Johnson <wyattjoh@gmail.com>
2016-11-16 22:04:43 -0700

    Fixed redis, changed color

--- redis.js
+++ redis.js
@@ -4,11 +4,12 @@ const url = process.env.REDIS_URL || 'redis://localhost';
 
 const client = redis.createClient(url, {
   retry_strategy: function(options) {
-    if (options.error.code === 'ECONNREFUSED') {
+    if (options.error && options.error.code === 'ECONNREFUSED') {
 
       // End reconnecting on a specific error and flush all commands with a individual error
       return new Error('The server refused the connection');
     }
+
     if (options.total_retry_time > 1000 * 60 * 60) {
 
       // End reconnecting after a specific timeout and flush all commands with a individual error
